5a443a7a61ca47242cb2c059cc3e59a45590de36
VoxAntiqua <andrew.padgett.88@gmail.com>
2024-09-11 14:39:10 -0400

    Added ScrollView to AppBar

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 
 import Constants from 'expo-constants';
 import theme from '../theme';
@@ -16,8 +16,10 @@ const styles = StyleSheet.create({
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab label="Repositories" linkTo="/" />
-      <AppBarTab label="Sign in" linkTo="/signin" />
+      <ScrollView horizontal>
+        <AppBarTab label="Repositories" linkTo="/" />
+        <AppBarTab label="Sign in" linkTo="/signin" />
+      </ScrollView>
     </View>
   );
 };
