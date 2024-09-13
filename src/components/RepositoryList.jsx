4caa2928c98916bc625daac6512ff161aa820ec3
VoxAntiqua <andrew.padgett.88@gmail.com>
2024-09-13 16:54:57 -0400

    Added jest for testing, refactored RepositoryList for testing purposes

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -10,23 +10,24 @@ const styles = StyleSheet.create({
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-const RepositoryList = () => {
-  const { repositories } = useRepositories();
-
-  // Get notes from edges array
+export const RepositoryListContainer = ({ repositories }) => {
   const repositoryNodes = repositories
     ? repositories.edges.map((edge) => edge.node)
     : [];
-
   return (
     <FlatList
       data={repositoryNodes}
       ItemSeparatorComponent={ItemSeparator}
       renderItem={({ item }) => <RepositoryItem item={item} />}
       keyExtractor={(item) => item.id}
-      // other props
     />
   );
+};
+
+const RepositoryList = () => {
+  const { repositories } = useRepositories();
+
+  return <RepositoryListContainer repositories={repositories} />;
 };
 
 export default RepositoryList;
