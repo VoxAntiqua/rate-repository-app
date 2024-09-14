d53e1c93406dbf90b05461f2620978c5e28bda42
VoxAntiqua <andrew.padgett.88@gmail.com>
2024-09-14 11:49:49 -0400

    Refactored SingleRepository to component directory, implemented review query

---
+++ src/components/SingleRepository/index.jsx
@@ -0,0 +1,89 @@
+import { useParams } from 'react-router-native';
+import { useQuery } from '@apollo/client';
+import { Pressable, StyleSheet, View, Linking } from 'react-native';
+import Text from '../Text';
+import { GET_REPOSITORY } from '../../graphql/queries';
+import theme from '../../theme';
+import { FlatList } from 'react-native-web';
+import RepositoryItem from '../RepositoryItem';
+
+const styles = StyleSheet.create({
+  button: {
+    height: 50,
+    borderRadius: 5,
+    marginBottom: 15,
+    marginHorizontal: 15,
+    backgroundColor: theme.colors.primary,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: theme.colors.appBarText,
+    fontSize: theme.fontSize.subheading,
+    fontWeight: theme.fontWeights.bold,
+  },
+  container: {
+    backgroundColor: theme.colors.itemBackground,
+  },
+  separator: {
+    height: 10,
+  },
+});
+
+const RepositoryInfo = ({ repository }) => {
+  const handleOpenGitHub = () => {
+    if (repository.url) {
+      Linking.openURL(repository.url);
+    }
+  };
+
+  return (
+    <>
+      <RepositoryItem item={repository} />
+      <View style={styles.container}>
+        <Pressable style={styles.button} onPress={handleOpenGitHub}>
+          <Text style={styles.buttonText}>Open in GitHub</Text>
+        </Pressable>
+      </View>
+    </>
+  );
+};
+
+const SingleRepository = () => {
+  const { repositoryId } = useParams();
+  const { data, loading, error } = useQuery(GET_REPOSITORY, {
+    variables: { repositoryId },
+  });
+
+  if (loading) {
+    return <Text>loading...</Text>;
+  }
+
+  if (error) {
+    return <Text>Error: {error.message}</Text>;
+  }
+
+  const repository = data?.repository;
+  const reviewNodes = repository.reviews
+    ? repository.reviews.edges.map((edge) => edge.node)
+    : [];
+  console.log(reviewNodes);
+
+  const ItemSeparator = () => <View style={styles.separator} />;
+
+  return (
+    <FlatList
+      data={reviewNodes}
+      ItemSeparatorComponent={ItemSeparator}
+      renderItem={({ item }) => (
+        <View>
+          <Text>{item.text}</Text>
+        </View>
+      )}
+      keyExtractor={(item) => item.id}
+      ListHeaderComponent={() => <RepositoryInfo repository={repository} />}
+    />
+  );
+};
+
+export default SingleRepository;
